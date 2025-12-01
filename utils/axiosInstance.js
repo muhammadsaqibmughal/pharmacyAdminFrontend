cb8f409bf2c7bd8bbbd47074c767e37f6fdd2a60
muhammadsaqibmughal <mrsoftware02@gmail.com>
2025-12-01 18:41:21 +0500

    updated

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pharmacybackend-ick9.onrender.com/api",
   withCredentials: true,
 });
 
